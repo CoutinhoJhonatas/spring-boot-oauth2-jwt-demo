ba0a9cb0acfe274a6d62682f7c1bf7a061f7360d
Jhonatas Coutinho <jhonatas.cjw@outlook.com>
2024-08-07 14:33:29 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$FiXhXlLt.rE7smTdwMMgLesULL0ej4t9d1lYJsS6nLisOXvVZvQyq');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$FiXhXlLt.rE7smTdwMMgLesULL0ej4t9d1lYJsS6nLisOXvVZvQyq');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
